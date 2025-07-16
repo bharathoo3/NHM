f554030f2a77d333f22a4134f097bb394f9bc7a1
rambh <rambharath979@gmail.com>
2025-07-17 05:20:51 +0530

    Removed old NHM folder from Git tracking

--- NHM/backend/models/Bookmark.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const BookmarkSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
-  url: { type: String, required: true }
-});
-
-module.exports = mongoose.model("Bookmark", BookmarkSchema);
